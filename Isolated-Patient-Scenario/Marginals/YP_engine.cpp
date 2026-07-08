f8c3d39778948243c48d226c50a23e29f2e74e79
marcusmarshall1511 <117832177+marcusmarshall1511@users.noreply.github.com>
2026-07-08 17:04:45 +0100

    Add files via upload

--- Isolated-Patient-Scenario/Marginals/YP_engine.cpp
+++ Isolated-Patient-Scenario/Marginals/YP_engine.cpp
@@ -89,6 +89,13 @@ NumericVector fast_trial_cpp_P(int nmaxmax, double gamma, double rC,
     std::unordered_map<std::string, double> VALUE_STORE;
     VALUE_STORE.reserve(1 << 20); // reserve 2^20 entries
     
+    auto get_val = [&](int l, int ehq, int ihq, int ehg, int ihg, int ep, int ip, int nn) {
+        std::string k = state_key(l, ehq, ihq, ehg, ihg, ep, ip, nn);
+        auto it = VALUE_STORE.find(k);
+        return (it != VALUE_STORE.end()) ? it->second : 0.0;
+    };
+
+
     NumericVector p_set;
     double P_val  = 0.0;
     
@@ -129,18 +136,26 @@ for (int nmax = 0; nmax <= nmaxmax; nmax++) {
                                     for (int iP = 0; iP <= iPmax; iP++){
                                       int eHQ = q-iHQ;
                                       int eHG = (h-q) - iHG;
+
+
+                                      std::string key_x = state_key(l_, eHQ, iHQ, eHG, iHG, eP, iP, n);
+                                        
+                                        // If it exists, skip
+                                        if (VALUE_STORE.count(key_x) > 0) continue;
+
+                                        
                                       double Gamma = gamma_HQ * iHQ + gamma_HG * iHG + gamma_P * iP;
-                                      double lambdaHQ = (NHQ - eHQ - iHQ) * (Beta_O[l_] + iHQ * Beta_HQ + iHG * Beta_H + iP * Beta_HQP);
-                                      double lambdaHG = (NHG - eHG - iHG) * (iHG * Beta_HG + iHQ * Beta_H + iP * Beta_HGP);
-                                      double lambdaP = (NP - eP - iP) * (iHQ * Beta_HQP + iHG * Beta_HGP);
-                                      double etaHQ = eHQ * zeta_HQ;
-                                      double etaHG = eHG * zeta_HG;
-                                      double etaP = eP * zeta_P;
+                                      double lambda_HQ = (NHQ - eHQ - iHQ) * (Beta_O[l_] + iHQ * Beta_HQ + iHG * Beta_H + iP * Beta_HQP);
+                                      double lambda_HG = (NHG - eHG - iHG) * (iHG * Beta_HG + iHQ * Beta_H + iP * Beta_HGP);
+                                      double lambda_P = (NP - eP - iP) * (iHQ * Beta_HQP + iHG * Beta_HGP);
+                                      double eta_HQ = eHQ * zeta_HQ;
+                                      double eta_HG = eHG * zeta_HG;
+                                      double eta_P = eP * zeta_P;
                                       double delta_iso = Hyp[l_];
                                       double mu_IP = iP * deltaP;
                                       double mu_EP = eP * deltaP;
 
-                                      double Theta = Gamma + lambdaHQ + lambdaHG + lambdaP + etaHQ + etaHG + etaP + delta_iso + mu_IP + mu_EP;
+                                      double Theta = Gamma + lambda_HQ + lambda_HG + lambda_P + eta_HQ + eta_HG + eta_P + delta_iso + mu_IP + mu_EP;
  
 
 
@@ -149,37 +164,52 @@ for (int nmax = 0; nmax <= nmaxmax; nmax++) {
                                         } else {
                                             VALUE_STORE[state_key(l_, eHQ, iHQ, eHG, iHG, eP, iP, n)] = 0.0;
                                         }
-                                        
-                                        if (Theta > 0){
 
-                                        if (n==0){
-                                            VALUE_STORE[state_key(l_, eHQ, iHQ, eHG, iHG, eP, iP, n)] = 
-                                            (Gamma +
-                                            lambdaHQ*VALUE_STORE[state_key(l_, eHQ+1, iHQ, eHG, iHG, eP, iP, n)] +
-                                            lambdaHG*VALUE_STORE[state_key(l_, eHQ, iHQ, eHG+1, iHG, eP, iP, n)] +
-                                            delta_iso*VALUE_STORE[state_key(l_-1, eHQ, iHQ, eHG, iHG, eP, iP, n)] +
-                                            etaHQ*VALUE_STORE[state_key(l_, eHQ-1, iHQ+1, eHG, iHG, eP, iP, n)] +
-                                            etaHG*VALUE_STORE[state_key(l_, eHQ, iHQ, eHG-1, iHG+1, eP, iP, n)] +
-                                            etaP*VALUE_STORE[state_key(l_, eHQ, iHQ, eHG, iHG, eP-1, iP+1, n)] +
-                                            mu_IP*VALUE_STORE[state_key(l_, eHQ, iHQ, eHG, iHG, eP, iP-1, n)] +
-                                            mu_EP*VALUE_STORE[state_key(l_, eHQ, iHQ, eHG,  iHG, eP-1, iP, n)])/Theta;
-                                        } else {
-                                            VALUE_STORE[state_key(l_, eHQ, iHQ, eHG, iHG, eP, iP, n)] =
-                                            (lambdaHQ*VALUE_STORE[state_key(l_, eHQ+1, iHQ, eHG, iHG, eP, iP, n)] +
-                                            lambdaHG*VALUE_STORE[state_key(l_, eHQ, iHQ, eHG+1, iHG, eP, iP, n)] +
-                                            lambdaP*VALUE_STORE[state_key(l_, eHQ, iHQ, eHG, iHG, eP+1, iP, n-1)] +
-                                            delta_iso*VALUE_STORE[state_key(l_-1, eHQ, iHQ, eHG, iHG, eP, iP, n)] +
-                                            etaHQ*VALUE_STORE[state_key(l_, eHQ-1, iHQ+1, eHG, iHG, eP, iP, n)] +
-                                            etaHG*VALUE_STORE[state_key(l_, eHQ, iHQ, eHG-1, iHG+1, eP, iP, n)] +
-                                            etaP*VALUE_STORE[state_key(l_, eHQ, iHQ, eHG, iHG, eP-1, iP+1, n)] +
-                                            mu_IP*VALUE_STORE[state_key(l_, eHQ, iHQ, eHG, iHG, eP, iP-1, n)] +
-                                            mu_EP*VALUE_STORE[state_key(l_, eHQ, iHQ, eHG,  iHG, eP-1, iP, n)])/Theta;
-                                        }} else { VALUE_STORE[state_key(l_, eHQ, iHQ, eHG, iHG, eP, iP, n)] =  0.0;
-                                          }
-                                    }
-                                }
+                                        if (n == 0){
+                                        
+                                        double term_gamma  = Gamma;
+                                        double term_deltaL = (l_ > 0)  ? get_val(l_-1, eHQ, iHQ, eHG, iHG, eP, iP, n) * delta_iso : 0.0;
+                                        double term_muE    = (eP > 0)  ? get_val(l_, eHQ, iHQ, eHG, iHG, eP-1, iP, n) * mu_EP : 0.0;
+                                        double term_muI    = (iP > 0)  ? get_val(l_, eHQ, iHQ, eHG, iHG, eP, iP-1, n) * mu_IP : 0.0;
+                                        double term_etaHQ  = (eHQ > 0) ? get_val(l_, eHQ-1, iHQ+1, eHG, iHG, eP, iP, n) * eta_HQ : 0.0;
+                                        double term_etaHG  = (eHG > 0) ? get_val(l_, eHQ, iHQ, eHG-1, iHG+1, eP, iP, n) * eta_HG : 0.0;
+                                        double term_etaP   = (eP > 0)  ? get_val(l_, eHQ, iHQ, eHG, iHG, eP-1, iP+1, n) * eta_P : 0.0;
+                    
+                                        double term_lambdaHQ = (NHQ - eHQ - iHQ > 0) ? get_val(l_, eHQ+1, iHQ, eHG, iHG, eP, iP, n) * lambda_HQ : 0.0;
+                                        double term_lambdaHG = (NHG - eHG - iHG > 0) ? get_val(l_, eHQ, iHQ, eHG+1, iHG, eP, iP, n) * lambda_HG : 0.0;
+                                        double term_lambdaP = (NP - eP - iP > 0) ? get_val(l_, eHQ, iHQ, eHG, iHG, eP+1, iP, n-1) * lambda_P : 0.0;
+                                        if (Theta > 0.0) {
+                                          double value = (1.0 / Theta) * (term_gamma + term_deltaL + term_muE + term_muI + term_etaHQ + term_etaHG + term_etaP + term_lambdaHQ + term_lambdaHG);
+                                          VALUE_STORE[key_x] = value;
+                                        
+                                        }
+                                    } else if (n > 0) {
+                                        
+                                        
+                                        
+                                        double term_deltaL = (l_ > 0)  ? get_val(l_-1, eHQ, iHQ, eHG, iHG, eP, iP, n) * delta_iso : 0.0;
+                                        double term_muE    = (eP > 0)  ? get_val(l_, eHQ, iHQ, eHG, iHG, eP-1, iP, n) * mu_EP : 0.0;
+                                        double term_muI    = (iP > 0)  ? get_val(l_, eHQ, iHQ, eHG, iHG, eP, iP-1, n) * mu_IP : 0.0;
+                                        double term_etaHQ  = (eHQ > 0) ? get_val(l_, eHQ-1, iHQ+1, eHG, iHG, eP, iP, n) * eta_HQ : 0.0;
+                                        double term_etaHG  = (eHG > 0) ? get_val(l_, eHQ, iHQ, eHG-1, iHG+1, eP, iP, n) * eta_HG : 0.0;
+                                        double term_etaP   = (eP > 0)  ? get_val(l_, eHQ, iHQ, eHG, iHG, eP-1, iP+1, n) * eta_P : 0.0;
+                    
+                                        double term_lambdaHQ = (NHQ - eHQ - iHQ > 0) ? get_val(l_, eHQ+1, iHQ, eHG, iHG, eP, iP, n) * lambda_HQ : 0.0;
+                                        double term_lambdaHG = (NHG - eHG - iHG > 0) ? get_val(l_, eHQ, iHQ, eHG+1, iHG, eP, iP, n) * lambda_HG : 0.0;
+                                        double term_lambdaP = (NP - eP - iP > 0) ? get_val(l_, eHQ, iHQ, eHG, iHG, eP+1, iP, n-1) * lambda_P : 0.0;
+                                        
+                                        if (Theta > 0.0) {
+                                            double value = (1.0 / Theta) * (term_deltaL + term_muE + term_muI + term_etaHQ + term_etaHG + term_etaP + term_lambdaHQ + term_lambdaHG+term_lambdaP);
+                                            VALUE_STORE[key_x] = value;
+                                        }
+                                            }
+                                        }
+                                        
+                                        
                             }
                         }
+                    }
+
                     }
                 }
             }
